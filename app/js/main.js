da8630de830aaf9117698a745b26ceebc29d3002
galvatron03 <orexow.valera@gmail.com>
2022-06-25 18:08:54 +0500

    finish header

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(function (){
+    // icon search
+    $("#search").click(function() {
+        $(".menu-item").addClass('hide-item');
+        $(".header_search-form").addClass('aсtive');
+        $(".close").addClass('active');
+        $("#search").hide();
+    })
+    $(".close").click(function() {
+        $(".menu-item").removeClass('hide-item');
+        $(".header_search-form").removeClass('aсtive');
+        $(".close").removeClass('active');
+        $("#search").show();
+    })
+
+    // Sticke scroll 
+    window.addEventListener("scroll", function(){
+        const header = document.querySelector(".header_top-inner");
+        header.classList.toggle("sticky", window.scrollY > 0)
+    })
+});
