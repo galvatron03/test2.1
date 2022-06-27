a7a6544e33fc5a4ca4fe1244d8bac184354f2fac
galvatron03 <orexow.valera@gmail.com>
2022-06-27 21:27:07 +0500

    finish sales-leaders

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task("style", function () {
 gulp.task("script", function () {
   return gulp
     .src([
-      "node_modules/slick-carousel/slick.js",
+      "node_modules/slick-carousel/slick/slick.js",
       "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
       "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
     ])
